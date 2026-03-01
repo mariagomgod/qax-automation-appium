0d8154241914741821d111a7cd92aa555adac473
mariagomgod <79453388+mariagomgod@users.noreply.github.com>
2026-03-01 14:38:46 +0100

    Appium - Stage 1 - WarmUp

---
+++ Stage_1/WarmUp/qa-automation/src/test/java/com/qaxpert/AptoideTesting.java
@@ -0,0 +1,30 @@
+package com.qaxpert;
+
+import java.net.URL;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class AptoideTesting {
+
+    public static void main(String[] args) {
+        try {
+
+            UiAutomator2Options options = new UiAutomator2Options()
+                    .setDeviceName("Pixel_9_Pro")
+                    .setPlatformName("Android")
+                    .setAutomationName("UiAutomator2")
+                    .setApp(AndroidAppiumDemo.class.getClassLoader().getResource("aptoide-testing.apk").getPath());
+
+            URL appiumServerUrl = new URL("http://localhost:4723/wd/hub");
+
+            AppiumDriver driver = new AndroidDriver(appiumServerUrl, options);
+
+            System.out.println("✅ Aptoide inicializado correctamente");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Stage_1/WarmUp/qa-automation/src/test/java/com/qaxpert/Navegador.java
@@ -0,0 +1,31 @@
+package com.qaxpert;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.URL;
+
+public class Navegador {
+
+    public static void main(String[] args) {
+        try {
+
+            UiAutomator2Options options = new UiAutomator2Options()
+                    .setDeviceName("Pixel_9_Pro")
+                    .setPlatformName("Android")
+                    .setAutomationName("UiAutomator2")
+                    .setAppPackage("com.android.chrome")
+                    .setAppActivity("com.google.android.apps.chrome.Main");
+
+            URL appiumServerUrl = new URL("http://localhost:4723/wd/hub");
+
+            AppiumDriver driver = new AndroidDriver(appiumServerUrl, options);
+
+            System.out.println("✅ Navegador inicializado correctamente");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
